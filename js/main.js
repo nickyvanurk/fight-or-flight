a805ce39b392e654b298c56c3e5edaf3261ee49c
Nicky van Urk <nickyvanurk@gmx.com>
2018-01-19 00:42:10 +0100

    Setup initial Vue structure

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    showControls: false,
+    showLog: false
+  },
+  methods: {
+    startGame() {
+      this.showControls = true;
+      this.showLog = true;
+    },
+    attack() {
+
+    },
+    specialAttack() {
+
+    },
+    heal() {
+
+    },
+    giveUp() {
+      this.showControls = false;
+    }
+  }
 });
