a4b76bc2ae6cd450de6b7519050009926ae87683
Nicky van Urk <nickyvanurk@gmx.com>
2018-01-19 01:19:05 +0100

    Add log functionality

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ new Vue({
     showControls: false,
     showLog: false,
     player: {health: 100, strength: 10, intellect: 10},
-    monster: {health: 100}
+    monster: {health: 100},
+    playerTurn: true,
+    log: []
   },
   methods: {
     startGame() {
@@ -18,15 +20,21 @@ new Vue({
 
     },
     heal() {
-      if (this.player.health == 100)
-        return;
-      else if (this.player.health > 90)
-        this.player.health = 100;
-      else
-        this.player.health += this.player.intellect;
+      if (this.player.health == 100) return;
+
+      let healingPower = this.player.health > 90
+                         ? 100 - this.player.health
+                         : this.player.intellect;
+
+      this.player.health += healingPower;
+      this.log.push({"Player heals himself for " + healingPower});
+      this.switchTurns();
     },
     giveUp() {
       this.showControls = false;
+    },
+    switchTurns() {
+      this.playerTurn = !this.playerTurn;
     }
   }
 });
